cebb6f79df83d72cc2b2c7909ae1f67cd8c67140
Antihayp <zayzsacha1998@gmail.com>
2019-03-23 16:58:04 +0700

    ->?

--- Task8/StartDocker/src/main/java/ru/eltex/StartDocker/Main.java
+++ Task8/StartDocker/src/main/java/ru/eltex/StartDocker/Main.java
@@ -7,14 +7,15 @@ import java.util.List;
 
 public class Main
 {
-	public static void main(String[] argv)
-	{
+	public static void main(String[] argv) {
 		DockerClient dockerClient = DockerClientBuilder.getInstance().build();
-		
+
 		List<Container> containers = dockerClient.listContainersCmd().exec();
 
 		for (Container con : containers) {
 			System.out.println(con);
 		}
+
+//		containers.forEach((a) -> System.out.println(a));
 	}
 }
